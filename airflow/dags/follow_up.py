5e1f851a157008179ce012ea871905aeaf50df64
Aneesh Makala <makalaaneesh@yahoo.com>
2020-05-24 22:21:50 +0530

    schedule follow up

--- airflow/dags/follow_up.py
+++ airflow/dags/follow_up.py
@@ -1,4 +1,6 @@
 from bot.airflow_helpers.replies import follow_up_with_replies
+from bot.airflow_helpers.db_helper import delete_docs_older_than
+from bot.airflow_helpers.replies import MONGODB_DB, MONGODB_COLLECTION_TWEET_REPLIES_SOURCE
 from bot.airflow_helpers.analytics import perform_analytics
 
 from airflow import DAG
@@ -18,15 +20,18 @@ default_args = {
     "retry_delay": timedelta(minutes=1),
 }
 
-NO_OF_REPLIES = 2
+DELETE_OLDER_TWEETS_DAYS = 2
 
 with DAG("follow_up",
          catchup=False,
          default_args=default_args,
-         schedule_interval="@hourly") as dag:
-    task1 = PythonOperator(task_id="follow_up_with_replies",
+         schedule_interval="0 */24 * * *") as dag:
+    task1 = PythonOperator(task_id="delete_older_tweets",
+                           python_callable=delete_docs_older_than,
+                           op_args=(MONGODB_DB, MONGODB_COLLECTION_TWEET_REPLIES_SOURCE, DELETE_OLDER_TWEETS_DAYS))
+    task2 = PythonOperator(task_id="follow_up_with_replies",
                            python_callable=follow_up_with_replies)
 
 
 
-task1
+task1 >> task2
